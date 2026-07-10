b7c233d910302104614fb65abca4918627c6edef
pauloperozocolombia@gmail.com <pauloperozocolombia@gmail.com>
2026-07-10 02:46:28 -0500

    Refactor hello_world and goodbye_world handlers to use consistent naming and update routing paths

--- src/home/handlers.rs
+++ src/home/handlers.rs
@@ -8,11 +8,21 @@ struct MessageResponse {
 }
 
 
-#[get("/")]
-async fn hello_word() -> HttpResponse {
+#[get("/say/hello")]
+async fn hello_world() -> HttpResponse {
 
     let response = MessageResponse {
         message: "Hello, World!".to_string(),
+    };  
+
+    HttpResponse::Ok().json(response)
+}
+
+#[get("/say/goodbye")]
+async fn goodbye_world() -> HttpResponse {
+
+    let response = MessageResponse {
+        message: "Goodbye, World!".to_string(),
     };  
 
     HttpResponse::Ok().json(response)
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use actix_web::{HttpServer,App};
 mod home;
-use home::handlers::hello_word;
+use home::handlers::{hello_world, goodbye_world};
  
 #[actix_web::main]
 async  fn main()-> std::io::Result<()>{
 
     HttpServer::new(|| {
-        App::new().service(hello_word)
+        App::new()
+        .service(hello_world)
+        .service(goodbye_world)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
